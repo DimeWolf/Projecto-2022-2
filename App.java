dc4d704fe992369a76eeca40114f384f584fb20d
DimeWolf <cardertro12@gmail.com>
2022-10-26 09:06:21 -0500

    a ver que sale

--- App.java
+++ App.java
@@ -1,15 +1,22 @@
 import java.util.Scanner;
 import java.util.InputMismatchException;
+import java.util.Random;
 
 public class App {
+
+
+     /// 250 - 500 W/h / 157 kW/h / 1000w = 1 kW / 247.5$ x 1 kW/h
      
      public static void main (String [] args) {
 
+          ///Calls
           Scanner newInput = new Scanner(System.in);
           Texto call = new Texto();
-          
-          int option;
-          boolean menu1  = false;
+          Random numberGen = new Random();
+
+          ///Variables
+          int option, precioKwH = 247, kWh, kWhtotal, consumoProm = 157, ProduccionMax = 500, ProduccionMin = 250, paneles = 1;
+          boolean menu1  = false, menu2 = false;
 
           while (!menu1) {
 
@@ -23,24 +30,50 @@ public class App {
                     switch (option) {
                          
                          case 1:
+                         menu2 = false;
 
-                         System.out.println("Nada");
+                         while (!menu2) {
+                              
+                              try {
+
+                                   kWh = numberGen.nextInt(ProduccionMax) + ProduccionMin;
+                                   kWhtotal = paneles * kWh; 
+
+                                   System.out.print("\n=========#   #=========\n\n- Usted tiene: " + paneles + " paneles solares, generando: " + kWhtotal + "Wh\n\n- El consumo energetico de una casa en colombia es de: " + consumoProm + "kWh costando: " + precioKwH + " pesos");
+
+                                   call.option_selection();
+
+                                   option = newInput.nextInt();
+
+                                   switch (option) {
+
+                                        case 1:
+
+                                        case 2:
+
+                                        menu2 = true;
+
+                                        default:
+
+                                        System.out.println("Porfavor ingrese solo los numeros indicados");
+                                        break;
+                                   }
+
+                              } catch (InputMismatchException e) {
+                                   System.out.println(":v");
+                              }
+                         }
+
                          break;
 
                          case 2:
 
-                         System.out.println("Nada, otra vez");
-                         break;
-
-                         case 3:
-
-                         System.out.println("Chao concha e su mare");
                          menu1 = true;
                          break;
 
                          default: 
 
-                         System.out.println("Cachon era tres numeros");
+                         System.out.println("Porfavor ingrese solo los numeros indicados");
                          break;
                     }
 
--- Texto.java
+++ Texto.java
@@ -2,11 +2,13 @@ public class Texto {
      
      ///Sip, esta clase es lo que parece, es para guardar todo el texto y menus en clases distintas que se puedan llamar multiples veces, en ves de repetirlo 1000 veces en la main class
 
+
      public void main_menu() {
-          System.out.print("\n=========== #Menu Principal #===========\n1. Nada\n2. Nada\n3. Nada pero saliendo\n");
+          System.out.print("\n===========# Menu Principal #===========\n1. Empezar\n2. Salir\n");
      }
 
      public void option_selection() {
-          System.out.print("\n======== #Seleccione una opcion# =======\n>\\");
+          System.out.print("\n========# Seleccione una opcion #=======\n>\\");
      }
+
 }
