da82c787add787370708b43bb3e50d6626d9f238
Dime <100039760+DimeWolf@users.noreply.github.com>
2022-10-31 15:09:55 -0500

    40% hecho

--- Texto.java
+++ Texto.java
@@ -2,7 +2,6 @@ public class Texto {
      
      ///Sip, esta clase es lo que parece, es para guardar todo el texto y menus en clases distintas que se puedan llamar multiples veces, en ves de repetirlo 1000 veces en la main class
 
-
      public void option_selection() {
           System.out.print("\n========# Seleccione una opcion #=======\n>\\");
      }
